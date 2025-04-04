7a1f64f1012cc2621064b8529a864e903c791fc7
Rosany Teran <rosanybteranm@gmail.com>
2025-04-04 16:32:56 -0700

    update upload file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,23 @@
+import tempfile
+
 import cv2
 import streamlit as st
 from pathlib import Path
 from ultralytics import YOLO
-
-st.set_page_config(
-    page_title="Object Detection using YOLOv8",
-    page_icon="🤖",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-st.title("Object Detection And Tracking using YOLOv8")
-st.sidebar.header("ML Model Config")
-model_type = st.sidebar.radio(
-    "Select Task", ['Detection', 'Segmentation'])
-confidence = float(st.sidebar.slider(
-    "Select Model Confidence", 25, 100, 40)) / 100
-if model_type == 'Detection':
-    model_path = Path('weights/yolov8n.pt')
-elif model_type == 'Segmentation':
-    model_path = Path('weights/yolov8n-seg.pt')
-
-VIDEOS_DICT = {
-    'video_1': 'videos/video_1.mp4',
-    'video_2': 'videos/video_2.mp4',
-    'video_3': 'videos/video_3.mp4',
-}
-
-source_vid = st.sidebar.selectbox(
-        "Choose a video...", VIDEOS_DICT.keys())
-
-display_tracker = st.sidebar.radio("Display Tracker", ('Yes', 'No'))
-tracker_type = st.sidebar.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
 
 def display_tracker_options():
     is_display_tracker = True if display_tracker == 'Yes' else False
     if is_display_tracker:
         return is_display_tracker, tracker_type
     return is_display_tracker, None
+
+
 def analysis_video(video_path, conf):
     model = YOLO(model_path)
     vid_cap = cv2.VideoCapture(video_path)
-    st_frame= st.empty()
+    st_frame = st.empty()
     is_display_tracker, tracker = display_tracker_options()
-    while (vid_cap.isOpened()):
+    while vid_cap.isOpened():
         success, image = vid_cap.read()
         if success:
             image = cv2.resize(image, (720, int(720 * (9 / 16))))
@@ -63,5 +38,33 @@ def analysis_video(video_path, conf):
         else:
             vid_cap.release()
             break
+
+
 if __name__ == '__main__':
-    analysis_video(VIDEOS_DICT.get(source_vid), confidence)
+
+    st.set_page_config(
+        page_title="Object Detection using YOLOv8",
+        page_icon="🤖",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+    st.title("Object Detection And Tracking using YOLOv8")
+    st.sidebar.header("ML Model Config")
+    model_type = st.sidebar.radio(
+        "Select Task", ['Detection', 'Segmentation'])
+    confidence = float(st.sidebar.slider(
+        "Select Model Confidence", 25, 100, 40)) / 100
+    if model_type == 'Detection':
+        model_path = Path('weights/yolov8n.pt')
+    elif model_type == 'Segmentation':
+        model_path = Path('weights/yolov8n-seg.pt')
+
+    display_tracker = st.sidebar.radio("Display Tracker", ('Yes', 'No'))
+    tracker_type = st.sidebar.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
+
+    uploaded_file = st.file_uploader("Upload a video", type=["mp4", "mov"])
+    if uploaded_file is not None:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(uploaded_file.read())
+            temp_path = tmp_file.name
+        analysis_video(temp_path, confidence)
